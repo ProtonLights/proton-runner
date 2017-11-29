6bba32cf0181ba366742796b7c518adde9eb672a
Ramith Jayatilleka <ramith.jayatilleka@gmail.com>
2017-11-28 19:57:15 -0600

    Fixed gitignore, removed music, switched to live instead of stdout in repl

--- src/main.rs
+++ src/main.rs
@@ -155,8 +155,8 @@ fn run_run_show(args: Args, cfg: Config) -> Result<(), Error> {
     let dmx_port = args.arg_dmx_port.unwrap();
     let plist_offset = args.arg_plist_offset.unwrap_or(0);
 
-    let dmx = dmx_output::Stdout;
-    // let dmx = dmx_output::Live::new(&dmx_port);
+    // let dmx = dmx_output::Stdout;
+    let dmx = dmx_output::Live::new(&dmx_port)?;
     let show = try!(Show::new(&cfg, &proj_name, plist_offset));
     println!("Ready!");
 
